63994b965ec4b4f96f96553c23f213764c1eb0cd
Dylan MacKay <dylan.p.mackay@gmail.com>
2021-09-05 14:53:38 -0500

    init
    NuCamp React bootcamp website build

---
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -0,0 +1,23 @@
+import { Component } from "react";
+
+// class CampsiteInfo extends Component {
+//     constructor(props){
+//         super(props);
+//     }
+//     render () {
+//         if (props.campsite) {
+//             return (
+//                 <div className = "row">
+//                     bootstrap row
+//                 </div>
+//             )
+//         } else {
+//             return (
+//                 <div>
+//                     empty div
+//                 </div>
+//             )
+//         }
+//     }
+// }
+// export default CampsiteInfo;
