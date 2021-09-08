d8c294e136bd8ef4a99c7cdea4c0f58df485ee50
Dylan MacKay <dylan.p.mackay@gmail.com>
2021-09-08 12:59:58 -0500

    week 2 workshop update

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -1,23 +1,57 @@
-import { Component } from "react";
+import React, { Component } from "react";
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
-// class CampsiteInfo extends Component {
-//     constructor(props){
-//         super(props);
-//     }
-//     render () {
-//         if (props.campsite) {
-//             return (
-//                 <div className = "row">
-//                     bootstrap row
-//                 </div>
-//             )
-//         } else {
-//             return (
-//                 <div>
-//                     empty div
-//                 </div>
-//             )
-//         }
-//     }
-// }
-// export default CampsiteInfo;
+class CampsiteInfo extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    renderCampsite() {
+        const campsite = this.props.campsite;
+        return(
+        <div className="col-md-5 m-1">
+            <Card>
+                <CardImg top src = {campsite.image} alt = {campsite.name} />
+                <CardBody>
+                    <CardTitle>
+                        {campsite.name} 
+                    </CardTitle>
+                    <CardText>
+                        {campsite.description}
+                    </CardText>
+                </CardBody>
+            </Card>
+        </div>
+        );
+    }
+
+    renderComments() {
+        const comments = this.props.campsite.comments
+        if(comments) {
+        return(
+            <div className="col-md-5 m-1">
+                <h4>Comments</h4>
+                {comments.map(
+                comment => 
+                <div key={comment.id}>
+                {comment.text} 
+                <br/> -- {comment.author}, 
+                    {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}<br/><br/>
+                </div>)}
+            </div>);
+        } return <div />;
+    }
+
+    render() {
+        if(this.props.campsite) {
+            return(
+                    <div className="row"> 
+                        {this.renderCampsite()}
+                        {this.renderComments()}
+                    </div>
+            );
+        } return <div />;
+    }
+
+  }
+export default CampsiteInfo;
